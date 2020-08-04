592d4211b867cad7466f8cd5b66ba1fc293907d5
Luc Perkins <luc@blockfi.com>
2020-08-04 16:46:57 -0700

    Add example package

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-  cmd = pkgs.runCommand "hello" {} ''
-    echo "Hello world"
+in {
+  hello = pkgs.writeScriptBin "hello" ''
+    #!${pkgs.stdenv.shell}
+
+    echo "Hello from my personal Nix channel!"
   '';
-in cmd
+}

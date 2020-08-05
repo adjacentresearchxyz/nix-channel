b334c512181841dd7ed7f79a781b51d49e547cbc
Luc Perkins <luc@blockfi.com>
2020-08-05 13:44:24 -0700

    Expose nixpkgs only

--- default.nix
+++ default.nix
@@ -2,9 +2,5 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
 in {
-  hello = pkgs.writeScriptBin "hello" ''
-    #!${pkgs.stdenv.shell}
-
-    echo "Hello from my personal Nix channel!"
-  '';
+  nixpkgs = pkgs;
 }

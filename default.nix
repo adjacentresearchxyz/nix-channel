c60496aa3ba958b767ce778dfe333e0798f03a58
Luc Perkins <luc@blockfi.com>
2020-08-05 14:39:22 -0700

    Add example overlay

--- default.nix
+++ default.nix
@@ -1,6 +1,17 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+
+  helloBin = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the Nix channel overlay!"
+  '';
+
+  helloOverlay = self: super: {
+    hello = helloBin;
+  };
+
+  pkgs = import sources.nixpkgs {
+    overlays = [ helloOverlay ];
+  };
 in {
   nixpkgs = pkgs;
 }
